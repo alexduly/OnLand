7c7cc53e2d6baad64eeaa624988c615967996928
Alex Duly <alexduly41@gmail.com>
2025-02-20 13:51:13 -0500

    feat: :sparkles: write points to file for further debug
    writing points to a file with land/water for further debug. leaving in for now. at least will liekly not do it in actual execution

--- preprocessing/testgen/testgen.py
+++ preprocessing/testgen/testgen.py
@@ -1,17 +1,18 @@
 from qgis.core import *
 import pandas as pd
 from tqdm import tqdm
+from qgis.PyQt.QtCore import QMetaType
 
-
-# add in a function to load test data via a file that runs if the proj is empty. will save me having to regernate grids every time 
+# add in a function to load test data via a file that runs if the proj is empty. will save me having to regernate grids every time
 
 
 def loadLayer():
-    output_layer = QgsVectorLayer("/workspaces/data/output/shapes/world.shp", baseName="output_layer", providerLib="ogr")
+    output_layer = QgsVectorLayer(
+        "/workspaces/data/output/shapes/world.shp", baseName="output_layer", providerLib="ogr")
     if not output_layer.isValid():
         print("Layer failed to load. Exiting...")
         exit(1)
-    else: 
+    else:
         print("Output shapefile loaded")
     return output_layer
 
@@ -30,88 +31,107 @@ def create_test_points(proj: QgsProject, points_count: int):
     if output == []:
         print("Unable to get layer, trying direct from file.")
         output_layer = loadLayer()
-    else: 
+    else:
         output_layer = output[0]
-    
 
-    pointsLayer: QgsVectorLayer = processing.run(algOrName="native:randompointsinextent",
+
+    points_layer: QgsVectorLayer = processing.run(algOrName="native:randompointsinextent",
                                                  parameters={"EXTENT": output_layer.extent(),
                                                              "POINTS_NUMBER": points_count,
                                                              "MIN_DISTANCE": 0,
                                                              "TARGET_CRS": output_layer.crs(),
                                                              "MAX_ATTEMPTS": 200,
                                                              "OUTPUT": "TEMPORARY_OUTPUT"})["OUTPUT"]
-    if not pointsLayer.isValid():
+    if not points_layer.isValid():
         print("Invalid points layer generated. Exiting...")
         exit(1)
     else:
         print("Points generated")
     rows = []
     # do not want the extent in this or eveyrthing will be considered on land
-    request = QgsFeatureRequest().setSubsetOfAttributes(['Grid'], output_layer.fields()).setFilterExpression('"Grid"!=\'Extent\'')
+    request = QgsFeatureRequest().setSubsetOfAttributes(
+        ['Grid'], output_layer.fields()).setFilterExpression('"Grid"!=\'Extent\'')
     ids = [f.id() for f in output_layer.getFeatures(request)]
     output_layer.selectByIds(ids)
-
 
     filtered_output_layer = QgsVectorLayer(
         "Polygon?crs=" + output_layer.crs().authid(), baseName="FilteredOutput", providerLib="memory")
     filtered_output_layer_data = filtered_output_layer.dataProvider()
-    
+
     # Add selected features to the new layer
     filtered_output_layer_data.addFeatures(output_layer.selectedFeatures())
-    
+
     filtered_output_layer.updateExtents()
 
-      
-    if output_layer.featureCount() - filtered_output_layer.featureCount() == 1:
-        print("Successfully removed extent polygon ")
-    else:
+    if not output_layer.featureCount() - filtered_output_layer.featureCount() == 1:
         print("Removing extent polygon failed... will not be able to classify points. Exiting...")
         exit(1)
-        
-  
 
     processing.run("native:selectbylocation",
-                                    {"INPUT": pointsLayer,
-                                    # the within ( figure out if you can get this via enum)
-                                    "PREDICATE": [6],
-                                    "INTERSECT":  filtered_output_layer,#QgsProcessingFeatureSourceDefinition(output_layer, selectedFeaturesOnly=True, featureLimit=-1, geometryCheck=QgsFeatureRequest.GeometryAbortOnInvalid), 
-                                    "METHOD": 1 # create new selectoin
-                                    })
+                   {"INPUT": points_layer,
+                    # the within ( figure out if you can get this via enum)
+                    "PREDICATE": [6],
+                    # QgsProcessingFeatureSourceDefinition(output_layer, selectedFeaturesOnly=True, featureLimit=-1, geometryCheck=QgsFeatureRequest.GeometryAbortOnInvalid),
+                    "INTERSECT":  filtered_output_layer,
+                    "METHOD": 1  # create new selection
+                    })
+
+    land_field = QgsField(name="Land", type=QMetaType.Bool)
+
+    points_layer.startEditing()
+    
+    points_layer.addAttribute(land_field)
+
+    points_layer.updateFields()
+    
+  
     
     rows = []
-    for pointFtr in tqdm(pointsLayer.selectedFeatures(), total=pointsLayer.selectedFeatureCount(), desc="Processing Land points", leave=True):
+    for points_feature in tqdm(points_layer.selectedFeatures(), total=points_layer.selectedFeatureCount(), desc="Processing Land points", leave=True):
         pointsFtr: QgsFeature
-        point:QgsPointXY = pointFtr.geometry().asPoint()
+        point: QgsPointXY = points_feature.geometry().asPoint()
+        points_feature["Land"] = True
+
+        if not output_layer.extent().contains(point):
+            print(f'point out of extent??{point}')
 
         rows.append({
             "Lat": point.y(),
             "Lng": point.x(),
             "Land": True,
         })
-    
-    
+        points_layer.updateFeature(points_feature)
 
-
-    pointsLayer.invertSelection()
-    for pointFtr in tqdm(pointsLayer.selectedFeatures(), total=pointsLayer.selectedFeatureCount(), desc="Processing Water points", leave=True):
+    points_layer.invertSelection()
+    for points_feature in tqdm(points_layer.selectedFeatures(), total=points_layer.selectedFeatureCount(), desc="Processing Water points", leave=True):
         pointsFtr: QgsFeature
-        point:QgsPointXY = pointFtr.geometry().asPoint()
+        point: QgsPointXY = points_feature.geometry().asPoint()
+        if not output_layer.extent().contains(point):
+            print(f'point out of extent??{point}')
+        points_feature["Land"] = False
 
         rows.append({
             "Lat": point.y(),
             "Lng": point.x(),
             "Land": False,
         })
-        
-    
+        points_layer.updateFeature(points_feature)
+
+
+    points_layer.commitChanges()
     df = pd.DataFrame(rows, columns=["Lat", "Lng", "Land"])
-    df = df.sample(frac = 1) # shuffle the rows so that the land/water points are mixed up 
+    # df = df.sample(frac = 1).round(5) # shuffle the rows so that the land/water points are mixed up
 
     df.to_csv("/workspaces/data/output/test_data/test_points.csv", index=False)
 
     # points.
+    # debug write to shape file
+    
 
-    return
+    options = QgsVectorFileWriter.SaveVectorOptions()
+    options.driverName = "ESRI Shapefile"
+    outputFile = "/workspaces/data/output/test_data/points.shp"
+    QgsVectorFileWriter.writeAsVectorFormatV3(
+        layer=points_layer, fileName=outputFile, transformContext=QgsCoordinateTransformContext(), options=options)
 
-
+    print(f"Processed points created and written to {outputFile}")
